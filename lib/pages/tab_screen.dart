feb762431f29fbae4f4c10537f2ef355b548cfa9
Илья Евсеенко <addminuser258@gmail.com>
2024-02-05 22:25:14 +0300

    Теперь вкладки листаются вместе с кнопками

--- lib/pages/tab_screen.dart
+++ lib/pages/tab_screen.dart
@@ -15,6 +15,17 @@ class _MaterialTabBarDemoState extends State<MaterialTabBarDemo>
   void initState() {
     super.initState();
     _tabController = TabController(length: 3, vsync: this);
+    _tabController.addListener(_handleTabChange);
+  }
+
+  @override
+  void dispose() {
+    _tabController.dispose();
+    super.dispose();
+  }
+
+  void _handleTabChange() {
+    setState(() {});
   }
 
   @override
@@ -42,15 +53,16 @@ class _MaterialTabBarDemoState extends State<MaterialTabBarDemo>
                 ),
               ],
             ),
-            Center(child: _buildDataTable()),
+            SingleChildScrollView(
+              scrollDirection: Axis.vertical,
+              child: Center(child: _buildDataTable()),
+            ),
           ],
         ),
         bottomNavigationBar: BottomNavigationBar(
           currentIndex: _tabController.index,
           onTap: (int index) {
-            setState(() {
-              _tabController.index = index;
-            });
+            _tabController.animateTo(index);
           },
           items: const [
             BottomNavigationBarItem(
@@ -74,25 +86,30 @@ class _MaterialTabBarDemoState extends State<MaterialTabBarDemo>
   Widget _buildDataTable() {
     return DataTable(
       columns: const [
-        DataColumn(label: Text('ID')),
-        DataColumn(label: Text('Name')),
-        DataColumn(label: Text('Status')),
+        DataColumn(label: Text('Пара')),
+        DataColumn(label: Text('Дисциплина')),
+        DataColumn(label: Text('Каб.')),
       ],
       rows: const [
         DataRow(cells: [
-          DataCell(Text('1')),
-          DataCell(Text('John')),
-          DataCell(Text('Active')),
+          DataCell(Text('2\n10:10 - 11:40')),
+          DataCell(Text('Физическая культура\nИванов И.В.')),
+          DataCell(Text('14')),
         ]),
         DataRow(cells: [
-          DataCell(Text('2')),
-          DataCell(Text('Jane')),
-          DataCell(Text('Inactive')),
+          DataCell(Text('2\n10:10 - 11:40')),
+          DataCell(Text('Физическая культура\nИванов И.В.')),
+          DataCell(Text('14')),
         ]),
         DataRow(cells: [
-          DataCell(Text('3')),
-          DataCell(Text('Bob')),
-          DataCell(Text('Active')),
+          DataCell(Text('2\n10:10 - 11:40')),
+          DataCell(Text('Физическая культура\nИванов И.В.')),
+          DataCell(Text('14')),
+        ]),
+        DataRow(cells: [
+          DataCell(Text('2\n10:10 - 11:40')),
+          DataCell(Text('Физическая культура\nИванов И.В.')),
+          DataCell(Text('14')),
         ]),
       ],
     );
